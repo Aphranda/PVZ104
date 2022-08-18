54de84cf325d0ca8e57064d4f536d7b5508656cb
li Dong <1044770247@qq.com>
2022-08-18 21:37:55 +0800

    1. 对扩展接口Reset进行修改，在大于180°时，正转进行复位，在小于180°时，反转进行复位，节省时间。

--- GTSStandardizationAPI/AutoGCExtend.cs
+++ GTSStandardizationAPI/AutoGCExtend.cs
@@ -11,7 +11,18 @@ namespace GTSStandardizationAPI
         // 原点复位，带系统偏置
         public static void Reset(this GTSApi gTS, Dimension dimension, double hightSpeed, double lowSpeed, double offset, int timeout = -1)
         {
-            gTS.MoveAbsolute(dimension, hightSpeed, 0, timeout);
+            MotionControl motionControl = new MotionControl();
+            double position;
+            gTS.GetPosition(dimension, out position);
+            if (position > 180)
+            {
+                gTS.MoveAbsolute(dimension, hightSpeed, 360, timeout);
+                motionControl.MotorZero(dimension);
+            }
+            else
+            {
+                gTS.MoveAbsolute(dimension, hightSpeed, 0, timeout);
+            }
             gTS.Home(dimension, lowSpeed, offset, timeout);
         }
         // 继续移动
